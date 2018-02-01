c0c858a2b59d337bbfade276db9456d80db6574e
Nicole Bishop <nicolejayne915@hotmail.com>
2018-02-01 15:47:59 -0600

    First Commit

---
+++ dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Username] VARCHAR(50) NOT NULL, 
+    [Password] VARCHAR(50) NOT NULL, 
+    [Address] VARCHAR(50) NOT NULL, 
+    [PhoneNumber] INT NOT NULL
+)
