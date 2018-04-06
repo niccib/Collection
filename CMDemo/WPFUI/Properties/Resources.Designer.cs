f434728a0d1e5925ffb6fdad4eace3557ceb919f
Nicole Bishop <nicolejayne915@hotmail.com>
2018-04-06 13:46:02 -0500

    practice work

--- CMDemo/WPFUI/Properties/Resources.Designer.cs
+++ CMDemo/WPFUI/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace LaPizzaPos.Properties
+namespace WPFUI.Properties
 {
 
 
@@ -44,7 +44,7 @@ namespace LaPizzaPos.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("LaPizzaPos.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WPFUI.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
