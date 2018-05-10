85a3e71e043d25cc1070b0c9fec678c529068102
Nicole Bishop <nicolejayne915@hotmail.com>
2018-05-10 09:03:11 -0500

    testing testing 123

---
+++ OrderPad/LaPizzaOrderPad/ViewModels/ShellViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Caliburn.Micro;
+using LaPizzaOrderPad.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace LaPizzaOrderPad.ViewModels
+{
+    public class ShellViewModel : Conductor<object>
+    {
+
+        private BindableCollection<FoodOrder> _order = new BindableCollection<FoodOrder>();
+        private FoodOrder _fullOrder;
+        private string _foodItem;
+        private double _price;
+
+        public ShellViewModel()
+        {
+            Menu.Add(new FoodOrder { FoodItem = "Large", Price = 16.00 });
+
+        }
+    }
+}
---
+++ WpfPos/WpfPos/ViewModels/ShellViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfPos.ViewModels
+{
+    public class ShellViewModel : Conductor<object>
+    {
+        private BindableCollection<Order> _order = new BindableCollection<Order>();
+    }
+    public ShellViewModel()
+    {
+        
+    }
+}
