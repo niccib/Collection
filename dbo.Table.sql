29b4a285ceceac51553da68bd9574b6fd84137ae
Nicole Bishop <31833857+niccib@users.noreply.github.com>
2018-05-24 12:51:56 -0500

    Delete dbo.Table.sql

--- dbo.Table.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Table]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [Username] VARCHAR(50) NOT NULL, 
-    [Password] VARCHAR(50) NOT NULL, 
-    [Address] VARCHAR(50) NOT NULL, 
-    [PhoneNumber] INT NOT NULL
-)
